87c733d2f4613e4d33706dcae59864d5c4d628ee
rupinderkaur-code <kaurinder235@gmail.com>
2020-11-16 19:36:24 -0500

    adapters

--- app/src/main/java/com/clothing/activities/ForgotPasswordActivity.java
+++ app/src/main/java/com/clothing/activities/ForgotPasswordActivity.java
@@ -14,7 +14,7 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
-        getSupportActionBar().setTitle("Forget Password");
+        getSupportActionBar().setTitle("Forget Password!!");
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
